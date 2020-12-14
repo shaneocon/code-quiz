21b12cfd6902fdfcff8b8f420a7c2acb90a4809a
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-13 17:09:44 -0800

    front page edits

--- script.js
+++ script.js
@@ -8,3 +8,90 @@
         // 2. Parantheses belong at th' beginnin' o' a function. (FALSE)
         // 3. Quations declare th' end o' a statement in Javascript. (FALSE)
         // 4. Lemons are good fer scurvy. (TRUE)
+
+
+
+
+
+// THIS IS FURTHER HTML CODED THAT NEEDS TO BE DROPPED INTO INDEX.HTML
+
+        // <!-- Question 2 ---------------------------->
+        //     <section>
+        //         <div class="question-text">
+        //             <span>Parantheses belong at th' beginnin' o' a function.</span>
+        //         </div>
+        //         <div class="option-list">
+        //             <div class="option">
+        //                 <span>True</span>
+        //                 <div class="icon cross"><i class="fas fa-times"></i></div>
+        //             </div>
+
+        //             <div class="option">
+        //                 <span>False</span>
+        //                 <div class="icon tick"><i class="fas fa-check"></i></div>
+        //             </div>
+        //         </div>
+        //     </section>
+
+        //      <!--QUIZ BOX FOOTER SECTION ------------------------->
+
+        //      <footer>
+        //         <div class="total-questions">
+        //             <span><p>2</p><p>Of</p><p>4</p><p>Riddles</p></span>
+        //         </div>
+        //         <button class="next-btn">Next Riddle</button>
+        //     </footer>
+
+        //     <!-- Question 3 --------------------------->
+        //     <section>
+        //         <div class="question-text">
+        //             <span>Quotations declare th' end o' a statement in Javascript.</span>
+        //         </div>
+        //         <div class="option-list">
+        //             <div class="option">
+        //                 <span>True</span>
+        //                 <div class="icon cross"><i class="fas fa-times"></i></div>
+        //             </div>
+                
+        //             <div class="option">
+        //                 <span>False</span>
+        //                 <div class="icon tick"><i class="fas fa-check"></i></div>
+        //             </div>
+        //         </div>
+        //     </section>
+
+        //      <!--QUIZ BOX FOOTER SECTION ------------------------->
+
+        //      <footer>
+        //         <div class="total-questions">
+        //             <span><p>3</p><p>Of</p><p>4</p><p>Riddles</p></span>
+        //         </div>
+        //         <button class="next-btn">Next Riddle</button>
+        //     </footer>
+
+        //     <!-- Question 4 --------------------------->
+        //     <section>
+        //         <div class="question-text">
+        //             <span>Lemons are good fer scurvy.</span>
+        //         </div>
+        //         <div class="option-list">
+        //             <div class="option">
+        //                 <span>True</span>
+        //                 <div class="icon tick"><i class="fas fa-check"></i></div>
+        //             </div>
+                
+        //             <div class="option">
+        //                 <span>False</span>
+        //                 <div class="icon cross"><i class="fas fa-times"></i></div>
+        //             </div>
+        //         </div>
+        //     </section>
+
+        //      <!--QUIZ BOX FOOTER SECTION ------------------------->
+
+        //      <footer>
+        //         <div class="total-questions">
+        //             <span><p>4</p><p>Of</p><p>4</p><p>Riddles</p></span>
+        //         </div>
+        //         <button class="next-btn">Next Riddle</button>
+        //     </footer>
